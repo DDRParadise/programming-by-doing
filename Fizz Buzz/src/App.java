a7885b0a726624a5136bd401dfc05423092eee84
Rachel Duxler <rachel@redefiningalldiets.com>
2021-01-03 15:00:24 -0800

    Fizz Buzz Project

---
+++ Fizz Buzz/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 100; i++) {
+			if (i % 3 == 0) {
+				System.out.println("fizz");
+			} else if (i % 5 == 0) {
+				System.out.println("buzz");
+			} else if (i % 15 == 0) {
+				System.out.println("fizzbuzz");
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+}
